00b13a614f00f6f0fab0dd01ec8848d7df05e849
Haydee Delgado <dhelgado723@gmail.com>
2026-07-05 17:43:53 -0500

    Actualizaciones

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from matrix_chain import matrix_chain_order
 def main():
     print("PARTICIONAMIENTO ÓPTIMO DE MATRICES")
+    print("PROGRAMACIÓN DINÁMICA")
     print("----------------------------------")
     n = int(input("Cantidad de matrices: "))
     dimensiones = []
@@ -9,12 +11,17 @@ def main():
         dimensiones.append(
             int(input(f"Dimension {i + 1}: "))
         )
+    inicio = time.perf_counter()
     costos = matrix_chain_order(dimensiones)
+    fin = time.perf_counter()
+
     print("\nTabla de costos mínimos:\n")
     for fila in costos:
         for valor in fila:
             print(f"{valor:<10}", end="")
         print()
     print("\nCosto mínimo de multiplicaciones:", costos[0][n - 1])
+    print(f"Tiempo de ejecución: {fin - inicio:.4f} segundos")
+
 if __name__ == "__main__":
     main()
